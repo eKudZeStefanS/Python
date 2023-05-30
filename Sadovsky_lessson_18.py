5113085cc53f1187c0ffe38329d739c22f174e2e
Stepan <stepansadovsky_ek@mail.ru>
2023-05-31 02:55:29 +0300

    *_*

--- Sadovsky_lessson_18.py
+++ Sadovsky_lessson_18.py
@@ -1,25 +1,32 @@
 class BankAccount:
- def init(self, account_number, balance):
-  self.__account_number = account_number
-  self.__balance = balance
+    def __init__(self, account_number, balance):
+        self.__account_number = account_number
+        self.__balance = balance
 
-  def get_account_number(self):
-      return self.__account_number
+    def get_account_number(self):
+        return self.__account_number
 
-  def get_balance(self):
-      return self.__balance
+    def get_balance(self):
+        return self.__balance
 
-  def deposit(self, amount):
-      if amount > 0:
-          self.__balance += amount
-          print("Депозит одобрен! Сейчас ваш счет составляет: ", self.__balance)
-      else:
-          print("Неверная сумма депозита!")
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            print("Депозит одобрен! Сейчас ваш счет составляет: ", self.__balance)
+        else:
+            print("Неверная сумма депозита!")
 
-  def withdraw(self, amount):
-      if amount > 0 and amount <= self.__balance:
-          self.__balance -= amount
-          print("Вывод одобрен! Сейчас ваш счет составляет:", self.__balance)
-      else:
-          print("Неверная сумма для вывода или недостаточно средств!")
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+            print("Вывод одобрен! Сейчас ваш счет составляет:", self.__balance)
+        else:
+            print("Неверная сумма для вывода или недостаточно средств!")
 
+
+account = BankAccount('123', 1000)
+print(account.get_account_number())
+print(account.get_balance())
+
+account.deposit(500)
+account.withdraw(200)
